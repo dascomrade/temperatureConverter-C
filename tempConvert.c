d059fd5a4284a28ac838cf5d2e01bda833e79d50
cmd <DeComrade@proton.me>
2026-03-30 21:32:29 -0500

    Add files via upload

--- tempConvert.c
+++ tempConvert.c
@@ -1,8 +1,9 @@
+#include <stdbool.h>
 #include <stdio.h>
 
 // make Celcius
-double f_to_c(double f) {  
-  double c = (double)5/9 * (f - 32);
+double f_to_c(double f) {
+  double c = (double)5 / 9 * (f - 32);
   printf("%d \n", c);
   return c;
 }
@@ -36,80 +37,84 @@ float f_to_k(float f) {
 }
 
 int main() {
+  int checkcont = true;
+  int conty = ' ';
+  bool cont = true;
+  while (cont == true) {
 
-  int cont = 0;
-  int valIn;
-  float outVal;
-  char outType = ' ';
-  char inType = ' ';
-  char inTypeFull[2] = "";
-  char outTypeFull[2] = "";
-  while (inType != 'F' && inType != 'C' && inType != 'K') {
-    printf("Enter the input type you want | F , C, K \n");
-    scanf("%s", &inType);
-  }
-
-  if (inType == 'F') {
-    inTypeFull[0] = *"Fahrenheit";
-  } else if (inType == 'C') {
-    inTypeFull[0] = *"Celcius";
-  } else {
-    inTypeFull[0] = *"Kelvin";
-  }
-
-
-  while (outType != 'F' && outType != 'C' && outType != 'K') {
-    printf("Enter the result type you want | F , C, K \n");
-    scanf("%s", &outType);
-  }
-  if (outType == 'F') {
-    outTypeFull[0] = *"Fahrenheit";
-  } else if (outType == 'C') {
-    outTypeFull[0] = *"Celcius";
-  } else {
-    outTypeFull[0] = *"Kelvin";
-  }
-
-  printf("Enter a value in %s \n", inTypeFull);
-  scanf("%d", &valIn);
-
-  if (inTypeFull[0] == 'F') { //From F
-    if (outTypeFull[0] == 'C') {  
-      outVal = f_to_c(valIn);
+    int valIn;
+    float outVal;
+    char outType = ' ';
+    char inType = ' ';
+    char inTypeFull[2] = "";
+    char outTypeFull[2] = "";
+    while (inType != 'F' && inType != 'C' && inType != 'K') {
+      printf("Enter the input type you want | F , C, K \n");
+      scanf("%s", &inType);
     }
-    else if(outTypeFull[0] == 'K'){
-      outVal = f_to_k(valIn);
+
+    if (inType == 'F') {
+      inTypeFull[0] = *"Fahrenheit";
+    } else if (inType == 'C') {
+      inTypeFull[0] = *"Celcius";
+    } else {
+      inTypeFull[0] = *"Kelvin";
+    }
+
+    while (outType != 'F' && outType != 'C' && outType != 'K') {
+      printf("Enter the result type you want | F , C, K \n");
+      scanf("%s", &outType);
+    }
+    if (outType == 'F') {
+      outTypeFull[0] = *"Fahrenheit";
+    } else if (outType == 'C') {
+      outTypeFull[0] = *"Celcius";
+    } else {
+      outTypeFull[0] = *"Kelvin";
+    }
+
+    printf("Enter a value in %s \n", inTypeFull);
+    scanf("%d", &valIn);
+
+    if (inTypeFull[0] == 'F') { // From F
+      if (outTypeFull[0] == 'C') {
+        outVal = f_to_c(valIn);
+      } else if (outTypeFull[0] == 'K') {
+        outVal = f_to_k(valIn);
+      }
+    } else if (inTypeFull[0] == 'K') { // From K
+      if (outTypeFull[0] == 'C') {
+        outVal = k_to_c(valIn);
+      } else if (outTypeFull[0] == 'F') {
+        outVal = k_to_f(valIn);
+      }
+    } else if (inTypeFull[0] == 'C') { // From C
+      if (outTypeFull[0] == 'F') {
+        outVal = c_to_f(valIn);
+      } else if (outTypeFull[0] == 'K') {
+        outVal = c_to_k(valIn);
+      }
+    }
+
+    // outVal = f_to_c(valIn);
+    printf("%d degrees %s is %f degrees %s \n", valIn, inTypeFull, outVal, outTypeFull);
+    printf(" Enter 0 to continue, and 1 to quit \n");
+    scanf("%i", &conty);
+    while (checkcont  == true) {
+      if (conty == 0) {
+        cont = true;
+        checkcont = false;
+        printf("\n \n------------------------------------------------------        \n \n");
+      }
+      else if(conty == 1){
+        checkcont = false;
+        cont = false;
+      }
+      else{
+        printf("either 1 or 0 \n");
+        checkcont = true;
+      }
     }
   }
-  else if (inTypeFull[0] == 'K') { // From K
-   if (outTypeFull[0] == 'C') {  
-      outVal = k_to_c(valIn);
-    }
-   else if(outTypeFull[0] == 'F'){
-      outVal = k_to_f(valIn);
-    }
-  } 
-  else if (inTypeFull[0] == 'C') { // From C
-    if (outTypeFull[0] == 'F') {  
-      outVal = c_to_f(valIn);
-    }
-    else if(outTypeFull[0] == 'K'){
-      outVal = c_to_k(valIn);
-    }
-  }
-
-   //outVal = f_to_c(valIn); 
-   printf("%d degrees %s is %f degrees %s \n", valIn, inTypeFull, outVal, outTypeFull);
-
-  printf("Continue? | 0 = no | 1-9 = yes |");
-
-  scanf("%d", &cont);
-  
-  if (cont != 0) {
-    main();
-  } else {
-    printf("Program Exited");
-  }
-
   return 0;
 }
